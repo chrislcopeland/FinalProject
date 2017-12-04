c3479884ad4e98713b31e2e56b260e89e4c957d7
Chris Copeland <chriscope44@gmail.com>
2017-12-04 16:06:35 -0500

    InitialCustomer
    First attempt at fleshing out Customer class

--- src/Customer.java
+++ src/Customer.java
@@ -1,4 +1,37 @@
-
+/**
+ * This class represents the customer
+ * @author hanisaf
+ */
 public class Customer {
+	int arriveTime = 0;
+	int duration = 0;
 
+	
+	/**
+	 * Constructor of the class
+	 * @param arriveTime customer arrival time
+	 * @param duration customer duration
+	 */
+	public Customer(int arriveTime, int duration) {
+      this.arriveTime = arriveTime;
+      this.duration = duration;
+	}
+	
+	/**
+	 * Getter method of arrival time
+	 * @return time of arrival
+	 */
+	public int getArriveTime() {
+	   // start here
+		return arriveTime;
+	}
+	
+	/**
+	 * Getter method of duration
+	 * @return duration
+	 */
+	public int getDuration() {
+	   // start here
+		return duration;
+	}
 }
