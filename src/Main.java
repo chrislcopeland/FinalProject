d1d61ad9eb12bde0cfa98c97ab2381711d302209
Chris Copeland <chriscope44@gmail.com>
2017-12-04 16:02:41 -0500

    Main
    Main method

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,22 @@
-
+import java.util.Scanner;
+/**
+ * 
+ * @author hanisaf
+ * This is the Main class that kick starts the program
+ * 
+ */
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	/**
+	 * YOU DON'T NEED TO CHANGE THIS METHOD
+	 */
+	public static void main(String[] args) throws Exception {
+		Scanner keyboard = new Scanner(System.in);
+		System.out.print("Please enter file name : ");
+		String fileName = keyboard.nextLine();
+		Scenario scenario = new Scenario(fileName);
+		scenario.run();
 	}
+	
 
 }
