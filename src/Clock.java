70a9ee059369532b5ad84fdb9af6abb9622a98a5
Chris Copeland <chriscope44@gmail.com>
2017-12-04 15:57:17 -0500

    Initial Clock Class Creating
    This was my first attempt at fleshing out the clock class as per Safadi's instructions. May revisit later.

--- src/Clock.java
+++ src/Clock.java
@@ -1,4 +1,40 @@
-
+/**
+ * @author hanisaf
+ * This class models the Clock that governs the progress of the simulation
+ */
 public class Clock {
-
+	private static int time = 0;
+	
+	/**
+	 * resets time to 0, also call printTime to display time
+	 */
+	public static void resetClock() {
+      time = 0;
+      printTime();
+	}
+	
+	/**
+	 * advance time by one minute, also call printTime to display time
+	 */
+	public static void tick() {
+      time++;
+      printTime();
+	}
+	
+	/**
+	 * 
+	 * @return the current time
+	 */
+	public static int getTime() {
+	   //start here
+		return time;
+	}
+	
+	/**
+	 * NO NEED TO CHANGE THIS METHOD
+	 * prints time to the console
+	 */
+	public static void printTime() {
+		System.out.println("Clock : time is " + time);
+	}
 }
